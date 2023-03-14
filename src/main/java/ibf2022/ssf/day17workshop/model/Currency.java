91c734287cb07e07d2085459aad1151c09bcd472
Woody3797 <woody3797@gmail.com>
2023-03-14 11:13:14 +0800

    day17workshop commit

---
+++ src/main/java/ibf2022/ssf/day17workshop/model/Currency.java
@@ -0,0 +1,7 @@
+package ibf2022.ssf.day17workshop.model;
+
+public class Currency {
+    
+    private String name;
+    
+}
---
+++ src/test/java/ibf2022/ssf/day17workshop/Day17workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.ssf.day17workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day17workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
